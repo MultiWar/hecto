7c96e17f33f21c8b8f520a5a96bba59377917bc0
Nicolas <nicolas.trevisol.quaresma@gmail.com>
2025-01-12 21:56:17 -0300

    Up to and including the 'Press Ctrl-Q to quit' section of chapter 3 of the tutorial

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,29 +1,8 @@
-use std::io::{self, Read};
+#![warn(clippy::all, clippy::pedantic)]
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    println!("Write away!");
-    for byte in io::stdin().bytes() {
-        match byte {
-            Ok(byte) => {
-                let c = byte as char;
-
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", byte);
-                } else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} \r", byte, c);
-                }
-
-                if c == 'q' {
-                    disable_raw_mode().unwrap();
-                    break;
-                }
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-            }
-        }
-    }
+    Editor::default().run();
 }
