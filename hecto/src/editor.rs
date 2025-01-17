a3dac7989dacf166e8389066157f85dc447746ac
Nicolas <nicolas.trevisol.quaresma@gmail.com>
2025-01-17 17:56:01 -0300

    Chapter 3, section 'clear the screen'

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,5 +1,7 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::execute;
+use std::io::stdout;
 
 pub struct Editor {
     should_quit: bool,
@@ -11,35 +13,58 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye! \r\n");
         }
 
-        print!("Goodbye! \r\n");
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent { code, modifiers, .. }) = event {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => ()
+            }
+        }
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        println!("Write away!");
-        
         loop {
-            if let Key(KeyEvent { code, modifiers, kind, state }) = read()? {
-                println!("code: {code:?}, modifiers: {modifiers:?}, kind: {kind:?}, state: {state:?} \r");
+            let event = read()?;
 
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => () 
-                }
-            }
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
 
             if self.should_quit {
                 break;
             }
         }
 
-        disable_raw_mode()?;
         Ok(())
     }
 }
